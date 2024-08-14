ae16d340d52c10d2cf3f1dc731ff461919254b5a
Nicolas Bisson <nicolasbisson14@gmail.com>
2024-08-13 22:22:01 -0400

    Music/Sfx managers

---
+++ src/system/game_settings/audio/sfx_manager.gd
@@ -0,0 +1,29 @@
+extends Node
+
+enum SfxName {
+	EXAMPLE,
+}
+
+var AUDIOFILES = {
+	SfxName.EXAMPLE: preload ("res://src/audio/gamblecore.mp3"),
+}
+
+
+var reference_player = AudioStreamPlayer.new()
+
+func _ready():
+	reference_player.bus = &"SFX"
+	play_audio(SfxName.EXAMPLE)
+
+
+func on_player_finished(player):
+	remove_child(player)
+
+
+func play_audio(sfx_name: SfxName):
+	var player = reference_player.duplicate()
+	var stream = AUDIOFILES.get(sfx_name)
+	player.set_stream(stream)
+	player.connect("finished", Callable(self,"on_player_finished").bind(player))
+	add_child(player)
+	player.play()
