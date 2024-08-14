34c41ac3fd9891b45496cb4588840d1631fe66dd
unknown <felix.gagne.cliche@gmail.com>
2024-08-14 03:45:46 -0400

    add icon texture edit

--- addons/theme_editor/theme_editor.gd
+++ addons/theme_editor/theme_editor.gd
@@ -4,14 +4,29 @@ class_name ThemeEditor
 extends Theme
 
 #region Exports
+@export var print_debug: bool = true
+
+@export var variants: Dictionary = {
+  "Button": ["Primary","Secondary","Accent"],
+  "CheckBox": ["Primary","Secondary","Accent"],
+  "CheckButton": ["Primary","Secondary","Accent"],
+  "HSlider": ["Primary","Secondary","Accent"],
+  "LineEdit": ["Primary","Secondary","Accent"],
+  "LinkButton": ["Primary","Secondary","Accent"],
+  "PanelContainer": ["Background","Surface","Primary","Secondary","Accent"],
+  "ProgressBar": ["Primary","Secondary","Accent"],
+  "TabBar": ["Background","Surface","Primary","Secondary","Accent"],
+  "TabContainer": ["Background","Surface","Primary","Secondary","Accent"],
+  "VSlider": ["Primary","Secondary","Accent"],
+}
+
 @export_group("Colors")
 @export var color_primary: Color = Color.WHITE
 @export var color_secondary: Color = Color.WHITE
 @export var color_accent: Color = Color.WHITE
 @export var color_background: Color = Color.WHITE
 @export var color_surface: Color = Color.WHITE
-@export var color_text_dark: Color = Color.WHITE
-@export var color_text_light: Color = Color.WHITE
+@export var color_text: Color = Color.WHITE
 
 @export_group("Typography")
 @export var font_body: Font
@@ -23,7 +38,6 @@ extends Theme
   "TypographyBody": {"font_type": "body", "font_size": 16},
   "TypographyLabel": {"font_type": "body", "font_size": 14},
 }
-
 #endregion
 
 func set_styles():
@@ -33,39 +47,31 @@ func set_styles():
   set_typography_styles()
 
   for theme_type in get_type_list():
-    set_stylebox_style(theme_type, color_background)
-    set_color_style(theme_type, get_text_color(color_background))
+    if !get_type_variation_base(theme_type):
+      set_stylebox_style(theme_type, color_background)
+      set_color_style(theme_type, color_text)
+      set_icon_style(theme_type, color_background)
 
-  set_button_variants()
-  set_panel_container_variants()
-  set_slider_variants()
-
-  get_icon_variant("CheckBox", color_primary)
+  set_variants()
   
   print("Styles applied")
 
-func set_panel_container_variants():
-  set_variant("Background", "PanelContainer", color_background)
-  set_variant("Surface", "PanelContainer", color_surface)
-  set_variant("Primary", "PanelContainer", color_primary)
-  set_variant("Secondary", "PanelContainer", color_secondary)
-  set_variant("Accent", "PanelContainer", color_accent)
-
-func set_button_variants():
-  set_variant("Primary", "Button", color_primary, color_text_light)
-  set_variant("Secondary", "Button", color_secondary, color_text_light)
-  set_variant("Accent", "Button", color_accent, color_text_light)
-
-func set_slider_variants():
-  set_variant("Primary", "HSlider", color_primary)
-  set_variant("Secondary", "HSlider", color_secondary)
-  set_variant("Accent", "HSlider", color_accent)
-  set_variant("Primary", "VSlider", color_primary)
-  set_variant("Secondary", "VSlider", color_secondary)
-  set_variant("Accent", "VSlider", color_accent)
-  set_variant("Primary", "ProgressBar", color_primary)
-  set_variant("Secondary", "ProgressBar", color_secondary)
-  set_variant("Accent", "ProgressBar", color_accent)
+func set_variants():
+  for variant_key in variants:
+    for variant in variants.get(variant_key):
+      match variant:
+        "Background":
+          set_variant(variant, variant_key, color_background, color_text)
+        "Surface":
+          set_variant(variant, variant_key, color_surface, color_text)
+        "Primary":
+          set_variant(variant, variant_key, color_primary, color_text)
+        "Secondary":
+          set_variant(variant, variant_key, color_secondary, color_text)
+        "Accent":
+          set_variant(variant, variant_key, color_accent, color_text)
+        _:
+          printerr("Unable to create variant %s for base type %s" % [variant, variant_key])
 
 func set_typography_styles():
   for key in typography_types:
@@ -84,14 +90,23 @@ func set_typography_styles():
     
     set_font_size("font_size", key, typography_type["font_size"])
 
-func set_variant(variant_name: String, base_type: String, bg_color: Color, color: Color = Color.TRANSPARENT):
+func set_variant(variant_name: String, base_type: String, bg_color: Color, color: Color):
   variant_name = base_type + variant_name
+  print(variant_name)
   add_type(variant_name)
   set_type_variation(variant_name, base_type)
-  set_stylebox_style(variant_name, bg_color)
 
-  if color != Color.TRANSPARENT:
+  if !get_stylebox_list(base_type).is_empty():
+    print("stylebox")
+    set_stylebox_style(variant_name, bg_color)
+
+  if !get_color_list(base_type).is_empty():
+    print("color")
     set_color_style(variant_name, color)
+
+  if !get_icon_list(base_type).is_empty(): 
+    print("icon")
+    set_icon_style(variant_name, bg_color)
 
 func set_color_style(theme_type: String, color: Color):
   var variant_base = get_type_variation_base(theme_type)
@@ -100,18 +115,28 @@ func set_color_style(theme_type: String, color: Color):
   if variant_base:
     colors = get_color_list(variant_base)
 
-  for color_name in colors:    
+  for color_name in colors:
     if color_name.contains("outline") || color_name.contains("shadow"):
       pass
 
-    elif  color_name.contains("disabled"):
-      var focus_color = get_color_alpha_variant(color, 0.5)
-      set_color(color_name, theme_type, focus_color)
+    elif color_name.contains("uneditable"):
+      set_color(color_name, theme_type, get_disabled_color(color))
+
+    elif theme_type.contains("LinkButton"):
+      if color_name.contains("disabled"):
+        set_color(color_name, theme_type, get_disabled_color(color))
+
+      elif color_name.contains("hover"):
+        set_color(color_name, theme_type, get_hover_color(color))
+
+      elif color_name.contains("pressed"):
+        set_color(color_name, theme_type, get_pressed_color(color))
 
     else:
       set_color(color_name, theme_type, color)
 
-    # print("Applied %s color type to %s/%s" % [color, theme_type, color_name])
+    if print_debug:
+      print("Applied %s color type to %s/%s" % [color, theme_type, color_name])
 
 func set_stylebox_style(theme_type: String, color: Color):
   var variant_base = get_type_variation_base(theme_type)
@@ -126,38 +151,78 @@ func set_stylebox_style(theme_type: String, color: Color):
       stylebox = get_stylebox(stylebox_name, variant_base).duplicate()
     
     if stylebox_name.contains("disabled"):
-      var disabled_color = get_color_alpha_variant(color, 0.5)
-      set_stylebox_color(theme_type, stylebox_name, stylebox, disabled_color)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, get_disabled_color(color))
 
     elif stylebox_name.contains("focus"):
-      var focus_color = color
-      set_stylebox_color(theme_type, stylebox_name, stylebox, focus_color)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, get_hover_color(color))
 
     elif stylebox_name.contains("hover"):
-      var hover_color = color.lightened(0.1)
-      set_stylebox_color(theme_type, stylebox_name, stylebox, hover_color)
+      var hover_color = color.lightened(0.24)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, get_hover_color(color))
 
     elif stylebox_name.contains("pressed"):
-      var pressed_color = color.lightened(0.2)
-      set_stylebox_color(theme_type, stylebox_name, stylebox, pressed_color)
+      var pressed_color = color.lightened(0.5)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, get_pressed_color(color))
       pass
 
     elif stylebox_name.contains("slider") || stylebox_name.contains("background"):
-      var slider_color = color.lightened(0.8)
-      set_stylebox_color(theme_type, stylebox_name, stylebox, get_color_alpha_variant(slider_color, 0.2))
+      var slider_color = color.lightened(0.67)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, get_color_alpha_variant(slider_color, 0.24))
       pass
 
     elif stylebox_name.contains("area_highlight"):
       var area_highlight_color = color.lightened(0.1)
-      set_stylebox_color(theme_type, stylebox_name, stylebox, area_highlight_color)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, area_highlight_color)
       pass
 
+    elif theme_type.contains("LineEdit"):
+      var line_edit_color = Color.TRANSPARENT
+      set_stylebox_color(stylebox_name, theme_type, stylebox, line_edit_color)
+      if stylebox_name.contains("read_only"):
+        (stylebox as StyleBoxFlat).border_color = get_disabled_color(color)
+      else:
+        (stylebox as StyleBoxFlat).border_color = color
+
+    elif theme_type.contains("Tab"):
+      set_stylebox_color(stylebox_name, theme_type, stylebox, color)
+      if stylebox_name.contains("tab_selected"):
+        (stylebox as StyleBoxFlat).border_color = get_pressed_color(color)
+      if stylebox_name.contains("tab_unselected"):
+        set_stylebox_color(stylebox_name, theme_type, stylebox, color.darkened(0.24))
+
     else:
-      set_stylebox_color(theme_type, stylebox_name, stylebox, color)
+      set_stylebox_color(stylebox_name, theme_type, stylebox, color)
+    
+    if print_debug:
+      print("Applied %s to stylebox type %s/%s" % [color, theme_type, stylebox_name])
 
-    # print("Applied %s to stylebox type %s/%s" % [color, theme_type, stylebox_name])
+func set_icon_style(theme_type: String, color: Color):
+  var variant_base = get_type_variation_base(theme_type)
+  var icons = get_icon_list(theme_type)
+ 
+  if variant_base:
+    icons = get_icon_list(variant_base)
 
-func set_stylebox_color(theme_type: String, stylebox_name: String, stylebox: StyleBox, color: Color):
+  if theme_type.contains("Slider"):
+    color = color.lightened(0.67)
+
+  for icon_name in icons:
+    var icon_texture = set_icon_color(icon_name, theme_type, color)
+
+    if icon_name.contains("disabled"):
+      var disabled_color = color_text
+      icon_texture = set_icon_color(icon_name, theme_type, disabled_color)
+      
+    set_icon(icon_name, theme_type, icon_texture)
+    
+    if print_debug:
+      print("Applied %s to icon %s/%s" % [color, theme_type, icon_name])
+
+func get_color_alpha_variant(color:Color, value: float):
+  color.a = value
+  return color
+
+func set_stylebox_color(stylebox_name: String, theme_type: String, stylebox: StyleBox, color: Color):
   if stylebox is StyleBoxFlat:
     stylebox.bg_color = color
     if stylebox.get_border_width_min() > 0:
@@ -170,27 +235,43 @@ func set_stylebox_color(theme_type: String, stylebox_name: String, stylebox: Sty
 
   set_stylebox(stylebox_name, theme_type, stylebox)
 
-func get_color_alpha_variant(color:Color, value: float):
-  color.a = value
-  return color
-
-func get_text_color(background_color: Color):
-  return color_text_light if ColorExtensions.rgb_to_oklab(background_color).l < 0.5 else color_text_dark
-
-func get_icon_variant(theme_type: String, color: Color):
-  var icons = get_icon_list(theme_type)
-  for icon_name in icons:
+func set_icon_color(icon_name: String, theme_type: String, color: Color):
+  var icon_dir = "res://src/ui/icons/%s" % [theme_type]
+  var variant_base = get_type_variation_base(theme_type)
+ 
+  if variant_base:
+    icon_dir = "res://src/ui/icons/%s" % [variant_base]
+  if !DirAccess.dir_exists_absolute(icon_dir):
+    DirAccess.make_dir_absolute(icon_dir)
+  
+  var image = Image.new()
+  var icon_path = "%s/%s.png" % [icon_dir, icon_name]
+  if FileAccess.file_exists(icon_path):
+    image = Image.load_from_file(icon_path)
+  else: 
     var texture = get_icon(icon_name, theme_type)
-    var image = texture.get_image()
-    for x in image.get_width():
-      for y in image.get_height():
-        var base_color = image.get_pixel(x,y)
-        var icon_color = color
-        if ColorExtensions.rgb_to_oklab(base_color).l < 0.5:
-          icon_color = icon_color.lightened(0.8)
-        var new_color = icon_color
-        new_color.a = color.a;
+    image = texture.get_image()
+    image.save_png(icon_path)
 
-        image.set_pixel(x, y, new_color)
-    var icon_texture = ImageTexture.new()
-    icon_texture.create_from_image(image)
+    if print_debug:
+      print("Saved new icon %s" % [icon_path])
+  
+  for x in image.get_width():
+    for y in image.get_height():
+      var base_color = image.get_pixel(x,y)
+      var icon_color = color
+      if ColorExtensions.rgb_to_oklab(base_color).l < 0.5:
+        icon_color = color.lightened(0.67)
+      icon_color.a = base_color.a;
+      image.set_pixel(x, y, icon_color)
+  
+  return ImageTexture.create_from_image(image)
+
+func get_hover_color(color: Color):
+  return color.lightened(0.24)
+
+func get_pressed_color(color: Color):
+  return color.lightened(0.5)
+
+func get_disabled_color(color: Color):
+  return get_color_alpha_variant(color.lightened(0.5), 0.67)
