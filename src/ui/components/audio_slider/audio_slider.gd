eea963676d4b3bef6bcb632d533c12bec45d54de
Nicolas Bisson <nicolasbisson14@gmail.com>
2024-08-15 22:03:20 -0400

    Fix indent

--- src/ui/components/audio_slider/audio_slider.gd
+++ src/ui/components/audio_slider/audio_slider.gd
@@ -5,9 +5,9 @@ extends HSlider
 var bus_index
 
 func _ready():
-	bus_index = AudioServer.get_bus_index(bus)
-	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
-	
+    bus_index = AudioServer.get_bus_index(bus)
+    value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+    
 
 func _value_changed(new_value):
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+    AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
